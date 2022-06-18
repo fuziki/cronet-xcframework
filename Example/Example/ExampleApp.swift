c79d85302836cda6b0810b358ffcf67fca84aaef
fuziki <hymne.shannon.projects@gmail.com>
2022-06-19 02:59:06 +0900

    create example

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by fuziki on 2022/06/19.
+//
+
+import MyLibrary
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/MyLibrary/Sources/MyLibrary/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by fuziki on 2022/06/19.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+    public init() {        
+    }
+    public var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
