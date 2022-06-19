71c189a8f017744a184e74f8945071672256e5fd
fuziki <hymne.shannon.projects@gmail.com>
2022-06-19 17:14:12 +0900

    fix template

--- template/_Package.swift
+++ template/_Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Cronet",
-            url: "https://github.com/fuziki/Cronet.xcframework/releases/download/VERSION/Cronet.xcframework.zip",
+            url: "https://github.com/fuziki/cronet-xcframework/releases/download/VERSION/Cronet.xcframework.zip",
             checksum: "CHECKSUM")
     ]
 )
