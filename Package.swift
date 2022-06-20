4bc08f2b34c8d6d8367405d1c36af668a289075c
runner <runner@Mac-1655693770338.local>
2022-06-20 05:21:36 +0000

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Cronet",
-            url: "https://github.com/fuziki/cronet-xcframework/releases/download/104.0.8-5112/Cronet.xcframework.zip",
-            checksum: "441f9a9e692dce4af93e8d849ba86ba0d332a58cd43586560eb3955963bac655")
+            url: "https://github.com/fuziki/cronet-xcframework/releases/download/103.0.5027-0/Cronet.xcframework.zip",
+            checksum: "5fa4d001f3b66b55b37d3e7dce78c4ebc77b992798a36a9a2344d65dd8b7f0fe")
     ]
 )
